c7d4cb5cb842109e311b38c330cebc2a12f3b2e0
Yatin <94924368+yatinnagar@users.noreply.github.com>
2022-07-04 19:33:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,25 +20,14 @@ app.get('/',(req,res)=>{
 
         // Get a random entry
         var random = Math.floor(Math.random() * count)
-      
-        // Again query all users but only fetch one offset by our random #
+        
         Post.findOne().skip(random).exec(
           function (err, result) {
-            // Tada! random user
-            console.log(result) 
             res.send(result)
-          })
-      })
-// Post.findOne().then(posts=>{
-//     if(!posts){
-//         const error=new Error("No post is there to fetch");
-//         error.statusCode=404;
-//         throw(error);
-//     }
-//     return res.status(200).json(posts);
-// }).catch(err=>console.log(err));
-}
-)
+          });
+      });
+
+});
 
 // Listening the app 
 app.listen(3000);
