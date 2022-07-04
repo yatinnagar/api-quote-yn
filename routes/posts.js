2fcda9c07e014cb00007d30a375deaea41615f74
Yatin <94924368+yatinnagar@users.noreply.github.com>
2022-07-04 21:33:57 +0530

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -3,12 +3,36 @@ const Post =require('../models/Post')
 const router=express.Router();
 router.get('/',async (req,res)=>{
     try{
-        const posts=await Post.find();
-        res.json(posts);
+        let charLimit=parseInt(req.query.char_limit);
+        if(charLimit){
+          
+            Post.find().$where(`this.quoteText===${charLimit}`).limit(1).exec((err,result)=>{
+              if(err){
+              return res.status(501).json({
+              error:"somthing went wrong!"
+              })
+              } else{
+              
+                  return res.json(result);
+              }
+            })
+            
+        } else {
+            
+         var random = Math.floor(Math.random() * count)
+        
+        Post.findOne().skip(random).exec(
+          function (err, result) {
+           return res.send(result)
+          });
+        
+        }
+        
     }catch(err)
     {
         res.json({message:err});
     }
 });
+
 
 module.exports=router;
